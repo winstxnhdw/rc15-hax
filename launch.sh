0a7bf3b1c20594a75a16b1d1ba23b65f0bf17a81
Winston H <56998716+winstxnhdw@users.noreply.github.com>
2022-05-15 04:43:42 +0800

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+if [["$1" == "dev"]]
+then
+  dotnet build rc15-hax
+fi
+
 submodules/SharpMonoInjector4.8/build/debug/smi.exe inject -p RobocraftClient -a rc15-hax/bin/Debug/net48/rc15-hax.dll -n RC15_HAX -c Loader -m Load
