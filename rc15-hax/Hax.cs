8fffa30f2375516de94070ec03b5adb977b967f5
winstxnhdw <winstxnhdw@gmail.com>
2022-05-14 18:26:09 +0800

    update

---
+++ rc15-hax/Hax.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Hax : MonoBehaviour
+{
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 200, 40), "This is a very useful cheat");
+    }
+}
---
+++ rc15-hax/Loader.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Loader : MonoBehaviour
+{
+    static GameObject mainGameObject;
+
+    public static void Load()
+    {
+        Loader.mainGameObject = new GameObject();
+        Loader.mainGameObject.AddComponent<Hax>();
+        DontDestroyOnLoad(Loader.mainGameObject);
+    }
+
+    public static void Unload()
+    {
+        Destroy(Loader.mainGameObject);
+    }
+}
+
