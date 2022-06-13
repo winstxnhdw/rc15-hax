c943c98f1fa76cc55e5d1056b8638746be06860b
winstxnhdw <winstxnhdw@gmail.com>
2022-06-14 01:36:54 +0800

    update sharpmonoinjector

--- launch.sh
+++ launch.sh
@@ -8,4 +8,4 @@ then
   dotnet build rc15-hax
 fi
 
-submodules/SharpMonoInjector4.8/build/debug/smi.exe inject -p RobocraftClient -a rc15-hax/bin/Debug/net48/rc15-hax.dll -n RC15_HAX -c Loader -m Load
+submodules/SharpMonoInjector4.8/SharpMonoInjector.Console/bin/debug/SharpMonoInjector4.8.exe inject -p RobocraftClient -a rc15-hax/bin/Debug/net48/rc15-hax.dll -n RC15_HAX -c Loader -m Load
