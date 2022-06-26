6c03f93a04fb3c2aa946de4a7c469c56cd805e81
winstxnhdw <winstxnhdw@gmail.com>
2022-06-27 00:15:31 +0800

    fix invalid switch error

--- docker_launch.bat
+++ docker_launch.bat
@@ -9,6 +9,8 @@ docker build -t %app_name% .
 docker run --name %app_name% %app_name%
 
 rmdir /s /q bin
+cls
+
 docker cp rc15-hax:/submodules/SharpMonoInjectorCore/bin/ .
 docker cp rc15-hax:/bin/rc15-hax.dll ./bin/
 start /WAIT /B ./bin/SharpMonoInjectorCore.exe inject -p RobocraftClient -a bin/rc15-hax.dll -n RC15_HAX -c Loader -m Load
--- launch.bat
+++ launch.bat
@@ -1,7 +1,8 @@
 @echo off
 
 rmdir /s /q bin
-rmdir /s /q ./rc15-hax/obj
+rmdir /s /q "rc15-hax/obj"
+cls
 
 dotnet build rc15-hax
 start /WAIT /B ./submodules/SharpMonoInjectorCore/bin/SharpMonoInjectorCore.exe inject -p RobocraftClient -a bin/rc15-hax.dll -n RC15_HAX -c Loader -m Load
