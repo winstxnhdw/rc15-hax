f082abe9ca2e4b921040b6512cc544371da53c9a
Winston H <56998716+winstxnhdw@users.noreply.github.com>
2022-07-05 04:37:52 +0800

    style changes

--- launch.bat
+++ launch.bat
@@ -5,6 +5,6 @@ rmdir /s /q "rc15-hax/obj"
 cls
 
 dotnet build rc15-hax
-start /WAIT /B ./submodules/SharpMonoInjectorCore/bin/SharpMonoInjectorCore.exe inject -p RobocraftClient -a bin/rc15-hax.dll -n RC15_HAX -c Loader -m Load
+start /wait /b ./submodules/SharpMonoInjectorCore/bin/SharpMonoInjectorCore.exe inject -p RobocraftClient -a bin/rc15-hax.dll -n RC15_HAX -c Loader -m Load
 
 pause
