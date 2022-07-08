bc7a156285dc41abc8a4c9d78ee10a290b1fde08
winstxnhdw <winstxnhdw@gmail.com>
2022-07-08 22:58:00 +0800

    small clean up

--- rc15-hax/Scripts/Utils/Reflector.cs
+++ rc15-hax/Scripts/Utils/Reflector.cs
@@ -4,19 +4,24 @@ using System.Reflection;
 
 namespace RC15_HAX;
 public class Reflector {
-    const BindingFlags InternalField = BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.GetField;
-    const BindingFlags InternalProperty = BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.GetProperty;
-    const BindingFlags InternalMethod = BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.InvokeMethod;
-    const BindingFlags StaticInternalField = BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.GetField;
-    const BindingFlags StaticInternalProperty = BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.GetProperty;
-    const BindingFlags StaticInternalMethod = BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.InvokeMethod;
+    const BindingFlags Internal = BindingFlags.NonPublic | BindingFlags.Instance;
+    const BindingFlags Public = BindingFlags.Public | BindingFlags.Instance;
+    const BindingFlags InternalStatic = BindingFlags.NonPublic | BindingFlags.Static;
+    const BindingFlags PublicStatic = BindingFlags.Public | BindingFlags.Static;
 
-    const BindingFlags PublicField = BindingFlags.Public | BindingFlags.Instance | BindingFlags.GetField;
-    const BindingFlags PublicProperty = BindingFlags.Public | BindingFlags.Instance | BindingFlags.GetProperty;
-    const BindingFlags PublicMethod = BindingFlags.Public | BindingFlags.Instance | BindingFlags.InvokeMethod;
-    const BindingFlags PublicStaticField = BindingFlags.Public | BindingFlags.Static | BindingFlags.GetField;
-    const BindingFlags PublicStaticProperty = BindingFlags.Public | BindingFlags.Static | BindingFlags.GetProperty;
-    const BindingFlags PublicStaticMethod = BindingFlags.Public | BindingFlags.Static | BindingFlags.InvokeMethod;
+    static BindingFlags InternalField => Reflector.Internal | BindingFlags.GetField;
+    static BindingFlags InternalProperty => Reflector.Internal | BindingFlags.GetProperty;
+    static BindingFlags InternalMethod => Reflector.Internal | BindingFlags.InvokeMethod;
+    static BindingFlags InternalStaticField => Reflector.InternalStatic | BindingFlags.GetField;
+    static BindingFlags InternalStaticProperty => Reflector.InternalStatic | BindingFlags.GetProperty;
+    static BindingFlags InternalStaticMethod => Reflector.InternalStatic | BindingFlags.InvokeMethod;
+
+    static BindingFlags PublicField => Reflector.Public | BindingFlags.GetField;
+    static BindingFlags PublicProperty => Reflector.Public | BindingFlags.GetProperty;
+    static BindingFlags PublicMethod => Reflector.Public | BindingFlags.InvokeMethod;
+    static BindingFlags PublicStaticField => Reflector.PublicStatic | BindingFlags.GetField;
+    static BindingFlags PublicStaticProperty => Reflector.PublicStatic | BindingFlags.GetProperty;
+    static BindingFlags PublicStaticMethod => Reflector.PublicStatic | BindingFlags.InvokeMethod;
 
     object Obj { get; }
     Type ObjType { get; }
@@ -106,7 +111,7 @@ public class Reflector {
     // Field extension methods
     public T GetInternalField<T>(string variableName) => this.GetField<T>(variableName, Reflector.InternalField);
 
-    public T GetInternalStaticField<T>(string variableName) => this.GetField<T>(variableName, Reflector.StaticInternalField);
+    public T GetInternalStaticField<T>(string variableName) => this.GetField<T>(variableName, Reflector.InternalStaticField);
 
     public T GetPublicField<T>(string variableName) => this.GetField<T>(variableName, Reflector.PublicField);
 
@@ -122,7 +127,7 @@ public class Reflector {
 
     public Reflector SetInternalField(string variableName, object value) => this.SetField(variableName, value, Reflector.InternalField);
 
-    public Reflector SetInternalStaticField(string variableName, object value) => this.SetField(variableName, value, Reflector.StaticInternalField);
+    public Reflector SetInternalStaticField(string variableName, object value) => this.SetField(variableName, value, Reflector.InternalStaticField);
 
     public Reflector SetPublicField(string variableName, object value) => this.SetField(variableName, value, Reflector.PublicField);
 
