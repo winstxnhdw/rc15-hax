9c7f3529b0dd12084aba726e43cdfa8302e0df3e
winstxnhdw <winstxnhdw@gmail.com>
2022-07-08 23:29:02 +0800

    clean

--- rc15-hax/Scripts/Utils/Hierarchy.cs
+++ rc15-hax/Scripts/Utils/Hierarchy.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 namespace RC15_HAX;
 public static class Hierarchy {
     public static void PrintAllAncestors(Transform transform) {
-        Console.Print($"Layer {transform.gameObject.layer}: {transform.name}");
+        Hierarchy.PrintTransformInfo(transform);
         if (transform.parent != null) Hierarchy.PrintAllAncestors(transform.parent);
     }
 
     public static void PrintAllDescendents(Transform transform) {
-        Console.Print($"Layer {transform.gameObject.layer}: {transform.name}");
+        Hierarchy.PrintTransformInfo(transform);
         foreach (Transform child in transform) Hierarchy.PrintAllDescendents(child);
     }
+
+    static void PrintTransformInfo(Transform transform) => Console.Print($"Layer {transform.gameObject.layer}: {transform.name}");
 }
