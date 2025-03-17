fce97254faf1f907c917146f38abf6b5c6b5ca0f
winstxnhdw <winstxnhdw@gmail.com>
2025-03-17 20:37:14 +0000

    build: remove `dnSpy`

--- requirements.bat
+++ requirements.bat
@@ -1,17 +1,4 @@
 @echo off
 
-set project_name=rc15-hax
-dotnet build %project_name%
+dotnet build rc15-hax
 dotnet publish submodules/SharpMonoInjectorCore
-
-:dev
-set /p choice=Would you like to build dnSpy? [y/N]:
-if /i '%choice%'=='Y' goto dnSpy
-echo Skipping dnSpy build..
-pause
-exit
-
-:dnSpy
-dotnet publish submodules/dnSpy -c Release -f net48
-robocopy submodules/dnSpy/dnSpy/dnSpy/bin/Release/net48 ./dnSpy /e /move /njh /njs /ndl /nc /ns
-pause
