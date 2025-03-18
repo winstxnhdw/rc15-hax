4efebf4535bdf4c3c0cf0f5a036dd00f08d8aaa8
Winston H. <56998716+winstxnhdw@users.noreply.github.com>
2025-03-18 07:53:11 +0000

    fix: injector is now published to `dist`

--- launch.bat
+++ launch.bat
@@ -1,6 +1,6 @@
 @echo off
 
 dotnet build rc15-hax
-start /wait /b ./submodules/SharpMonoInjectorCore/bin/SharpMonoInjector.exe inject -p RobocraftClient -a bin/rc15-hax.dll -n Hax -c Loader -m Load
+start /wait /b ./submodules/SharpMonoInjectorCore/dist/SharpMonoInjector.exe inject -p RobocraftClient -a bin/rc15-hax.dll -n Hax -c Loader -m Load
 
 pause
