654c77d5afe2d2c8b30f35fb5d183efa9ea5511f
Winston H. <56998716+winstxnhdw@users.noreply.github.com>
2025-03-18 07:54:37 +0000

    build(docker): update injector outdir in docker script

--- docker_launch.bat
+++ docker_launch.bat
@@ -11,8 +11,8 @@ docker run --name %app_name% %app_name%
 rmdir /s /q bin
 cls
 
-docker cp rc15-hax:/submodules/SharpMonoInjectorCore/bin/ .
+docker cp rc15-hax:/submodules/SharpMonoInjectorCore/dist/ .
 docker cp rc15-hax:/bin/rc15-hax.dll ./bin/
-start /wait /b ./bin/SharpMonoInjector.exe inject -p RobocraftClient -a bin/rc15-hax.dll -n Hax -c Loader -m Load
+start /wait /b ./dist/SharpMonoInjector.exe inject -p RobocraftClient -a bin/rc15-hax.dll -n Hax -c Loader -m Load
 
 pause
